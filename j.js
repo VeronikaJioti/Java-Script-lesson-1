6a23d4a6dfdc8ec7d74fb59ae0becf27ce211c90
VeronikaJioti <v.jioeva@gmail.com>
2022-06-02 09:39:41 +0400

    init: create file

---
+++ j.js
@@ -0,0 +1,19 @@
+// primitiuli cvladebi ar xdeba am cvladebis mutacia
+
+var name1 = 'geolab';  // js cvladis declare ar aris saWiro
+// Sevqmeni cvladi name da mivaniWe mniSvneloba geolab
+var name_undefined; // Semvqmeni cvladi name da aris undefined
+var number = 123; //integer
+var stringi = "abc"; //stingia
+var boolean = false; //boolean true an false
+var nullable = null; //NULL
+var symoblic = Symbol; //symbol
+
+// example
+
+var name2 = name1;
+
+console.log(name2);
+
+
+
